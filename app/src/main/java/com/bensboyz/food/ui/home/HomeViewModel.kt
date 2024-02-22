e9a4ac0669b7cfc3deefff60a04358afed14cd82
Adam $. Amir <64651604+Adam-S-Amir@users.noreply.github.com>
2024-02-22 11:35:50 -0500

    Create base application

---
+++ app/src/main/java/com/bensboyz/food/ui/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.bensboyz.food.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
