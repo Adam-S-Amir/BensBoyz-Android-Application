5179245355070d44c2fd8052137b464eace23be5
Adam $. Amir <64651604+Adam-S-Amir@users.noreply.github.com>
2024-02-28 09:17:39 -0500

    Create contact page

---
+++ app/src/main/java/com/bensboyz/food/ui/contact/ContactViewModel.kt
@@ -0,0 +1,7 @@
+package com.bensboyz.food.ui.contact
+
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/bensboyz/food/ui/singleview/SingleViewViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.bensboyz.food.ui.singleview
-
-import androidx.lifecycle.ViewModel
-
-class SingleViewViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
